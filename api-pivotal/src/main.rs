989fea949eb2ad9293fdda7a09b25b0c1e17a6da
pedro-git-projects <pedro.generic.email@gmail.com>
2022-10-26 20:30:46 -0300

    [183644879 - Delivered] changed html text

--- api-pivotal/src/main.rs
+++ api-pivotal/src/main.rs
@@ -5,15 +5,8 @@ fn app() -> Html {
     html! {
     <>
         <h1>{ "Trabalho Pivotal Tracker + API" }</h1>
-        <div>
-            <h3>{"Lorem Picsum:"}</h3>
-            <p>{ "Descrição da imagem aleatória 1" }</p>
-            <p>{ "Descrição da imagem aleatória 2" }</p>
-            <p>{ "Descrição da imagem aleatória 3" }</p>
-            <p>{ "Descrição da imagem aleatória 4" }</p>
-        </div>
-        <div>
-            <h3>{ "Imagem atual" }</h3>
+       <div>
+            <h3>{ "Recarregue para ver uma nova imagem:" }</h3>
             <img src="https://picsum.photos/200/300" alt="Lorem Picsum" />
         </div>
     </>
