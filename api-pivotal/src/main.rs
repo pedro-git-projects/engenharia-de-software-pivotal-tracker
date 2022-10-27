0afeb75c07aac4bacbc2047489a6710d0f3faa1a
pedro-git-projects <pedro.generic.email@gmail.com>
2022-10-26 23:09:25 -0300

    [183645516 Delivered] create cat picture struct && iterator

--- api-pivotal/src/main.rs
+++ api-pivotal/src/main.rs
@@ -1,15 +1,52 @@
 use yew::prelude::*;
 
+pub struct CatPicture {
+    pub id: String,
+    pub url: String,
+    pub width: usize,
+    pub height: usize,
+}
+
 #[function_component(App)]
 fn app() -> Html {
+    let pictures = vec![
+        CatPicture {
+            id: "8ji".to_string(),
+            url: "https://cdn2.thecatapi.com/images/8ji.jpg".to_string(),
+            width: 500,
+            height: 334,
+        },
+        CatPicture {
+            id: "OeOUzmQIk".to_string(),
+            url: "https://cdn2.thecatapi.com/images/OeOUzmQIk.jpg".to_string(),
+            width: 1080,
+            height: 1080,
+        },
+        CatPicture {
+            id: "22d".to_string(),
+            url: "https://cdn2.thecatapi.com/images/22d.jpg".to_string(),
+            width: 500,
+            height: 313,
+        },
+    ];
+
+    let pictures = pictures
+        .iter()
+        .map(|picture| {
+            html! {
+                <img src= {format!("{}", picture.url)}/>
+            }
+        })
+        .collect::<Html>();
+
     html! {
-    <>
-        <h1>{ "Trabalho Pivotal Tracker + API" }</h1>
-       <div>
+        <>
+            <h1>{ "Trabalho Pivotal Tracker + API" }</h1>
+            <div>
             <h3>{ "Recarregue para ver uma nova imagem:" }</h3>
-            <img src="https://picsum.photos/200/300" alt="Lorem Picsum" />
-        </div>
-    </>
+            {pictures}
+            </div>
+            </>
     }
 }
 
